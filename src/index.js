e295d0fd128e6d56c77f158f19592f9da8f27406
Elias Fournaridis <ilias@e-fresh.gr>
2021-06-30 19:08:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
-import Datepicker from './components/Datepicker.vue';
-import './assets/scss/app.scss'
 
+import Datepicker from './components/Datepicker.vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import _ from 'lodash';
+
+import './assets/scss/app.scss'
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.component('datepicker', Datepicker);
 
 export default Datepicker;
